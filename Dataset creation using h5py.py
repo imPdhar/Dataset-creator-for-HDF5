3f1f7a9137f09b5ad0ff39ac3e418e17e3d3259c
Bapu Pruthvidhar <talktobpd@icloud.com>
2020-08-06 21:32:00 +0530

    Add files via upload

---
+++ Dataset creation using h5py.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
+import h5py
+import os
+import glob
+import numpy as np
+from io import BytesIO
+from skimage.io import imread
+import matplotlib.pylab as plt
+from tqdm.auto import tqdm
+
+
+images = 'C:\\Users\\Bapu Pruthvidhar\\Desktop\\Alien vs Predator classifier\\data'
+dataset_name = 'C:\\Users\\Bapu Pruthvidhar\\Desktop\\Alien vs Predator classifier\\dataset.h5'
+
+
+
+def list_images(data_dir, name):
+    images = []
+    path = os.path.join(data_dir, name)
+    for img_type in ['png', 'jpg', 'jpeg']:
+        images += glob.glob(f'{path}/**/*.{img_type}')
+    return images
+
+
+def file2uint(fpath):
+    with open(fpath, 'rb') as file:
+        data = file.read()
+        return np.frombuffer(data, dtype='uint8')
+
+
+def create_dataset(data_dir, dataset_name):
+    train_imgs = list_images(data_dir, 'train')
+    val_imgs = list_images(data_dir, 'validation')
+    #test_imgs = list_images(data_dir, 'test')
+
+    # Define special type with dynamic length
+    dt = h5py.special_dtype(vlen=np.dtype('uint8'))
+
+    # Create datasets
+    with h5py.File(dataset_name, 'w') as out:
+        out.create_dataset("x_train", (len(train_imgs),), dtype=dt)
+        out.create_dataset("x_val", (len(val_imgs),), dtype=dt)
+        #out.create_dataset("x_test", (len(test_imgs),), dtype=dt)
+
+    # Write data
+    with h5py.File(dataset_name, 'a') as dset:
+        for i, img in enumerate(tqdm(train_imgs, 'Processing train set..')):
+            dset['x_train'][i] = file2uint(img)
+
+        for i, img in enumerate(tqdm(val_imgs, 'Processing val set..')):
+            dset['x_val'][i] = file2uint(img)
+
+        #for i, img in enumerate(tqdm(val_imgs, 'Processing test set..')):
+            #dset['x_test'][i] = file2uint(img)
+
+
+if __name__ == "__main__":
+
+    create_dataset(images, dataset_name)
+
+    # Test
+    hf = h5py.File(dataset_name, 'r')
+    img_data = hf['x_train'][0]
+    decoded_img = imread(BytesIO(img_data))
+    hf.close()
+    
+
+    plt.axis('off')
+    plt.imshow(decoded_img)
+    plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
